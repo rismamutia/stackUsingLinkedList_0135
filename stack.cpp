6ed8350c6e84be829987c386519c3f81e349798a
rismamutia <rismamutiadewi89@gmail.com>
2025-06-05 14:58:31 +0700

    membuat fungsi peek untuk melihat elemen paling atas dari stack

--- stack.cpp
+++ stack.cpp
@@ -55,4 +55,10 @@ public:
         cout << "Popped value: " << top->data << endl;
         top = top->next; //update the top pointer to the next node
     }
+
+    //Peek/top operation: Retrieve the value of the topmost element without removing it
+    void peek()
+    {
+        
+    }
 }
