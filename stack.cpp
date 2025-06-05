45f7f73934cb926ef5e6e773e778c50ba27932a9
rismamutia <rismamutiadewi89@gmail.com>
2025-06-05 15:10:07 +0700

    membuat inputan untuk pilihan

--- stack.cpp
+++ stack.cpp
@@ -90,7 +90,16 @@ int main()
         cout << "3. Peek\n";
         cout << "4. Exit\n";
         cout << "Enter your choise: ";
-        
+        cin >> choise;
+
+        switch(choise)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); //push the enterred value onto the stack
+
+        }
     }  
      
 
